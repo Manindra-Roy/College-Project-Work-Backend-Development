94c48e8d5b38274ae812c90ac581fae97a87574c
Sgg349 <158570319+Sgg349@users.noreply.github.com>
2024-04-30 23:03:11 +0530

    d

--- I-Tube Backend/static/js/pre-loader.js
+++
@@ -1,8 +0,0 @@
-const preLoader = () => {
-    setTimeout(() => {
-      document.querySelector(".page-loading-animation").style.opacity = "0";
-    }, 250);
-    setTimeout(() => {
-      document.querySelector(".page-loading-animation").style.display = "none";
-    }, 500);
-  };
